74fc4df125382ec1a1312a173080c06c4fb4f8a6
nguyenminhhieu28092001 <nguyenminhhieuk3@gmail.com>
2020-04-22 21:13:40 +0700

    laptrinhtaif8

---
+++ SHOOPEE/essets/js/main.js
@@ -0,0 +1,5 @@
+function taikhoan() {
+    var offtk = '<li class="header__nav-item"><a href="" class="header__nav-item-link header__nav-item-gach-2 "><b>Đăng ký</b> </a></li><li class="header__nav-item"><a href="" class="header__nav-item-link"> <b> Đăng nhập </b></a></li>';
+    document.getElementById('info_login').innerHTML = offtk;
+    //document.getElementById('dang_xuat').innerHTML=null;
+}
